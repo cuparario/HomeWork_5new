f90208281e3da588e425a1b91cfe40539794a68e
Рамиль Шарапов <cuparario@yandex.ru>
2023-12-05 15:03:08 +0300

    Добавили домашнюю работу 5

---
+++ Task_02/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 2: Задайте двумерный массив. Напишите программу, которая 
+поменяет местами первую и последнюю строку массива. */
+
+using System;
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        var array = new int[4, 4]
+            { { 1, 2, 3, 4 },
+              { 5, 6, 7, 8 },
+              { 9, 10, 11, 12 },
+              { 13, 14, 15, 16 }
+            };
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                var tmp = array[3, i];
+                array[3, i] = array[0, i];
+                array[0, i] = tmp;
+            }
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_03/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов. */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] array = 
+        {
+            {9, 2, 3, 1},
+            {4, 5, 6, 3},
+            {7, 1, 1, 5}
+        };           
+
+        int minSum = int.MaxValue;
+        int minRow = -1;
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum += array[i, j];
+            }
+
+            if (sum < minSum)
+            {
+                minSum = sum;
+                minRow = i;
+            }
+        }
+
+        Console.WriteLine($"Строка с минимальной суммой элементов: {minRow}");
+    }
+    
+}          
